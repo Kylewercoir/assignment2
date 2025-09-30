3506b53c55726e4c8a032790e5d1c6d2020d2c9f
Kylewercoir <Kylewercoir@gmail.com>
2025-09-30 19:02:04 -0400

    adding sql script to the file

---
+++ assignment 2 scirpt.sql
@@ -0,0 +1,12 @@
+CREATE TABLE orders (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    customer_name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    phone VARCHAR(20) NOT NULL,
+    pizza_size ENUM('Small','Medium','Large') NOT NULL,
+    toppings TEXT,
+    crust ENUM('Thin','Regular','Thick') NOT NULL,
+    notes TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+b
