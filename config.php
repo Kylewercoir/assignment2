e983e195763fd8ea534f8820b7fea2ee1c88fa7a
Kylewercoir <Kylewercoir@gmail.com>
2025-09-30 16:27:54 -0400

    finsihed config page

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+// Database connection settings
+define("DB_HOST", "172.31.22.43");
+define("DB_NAME", "Kyle200431463");
+define("DB_USER", "Kyle200431463");
+define("DB_PASS", "jVwfoehVCC");
+
+
