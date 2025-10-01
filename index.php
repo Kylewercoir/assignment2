0809a909c5d59890879635d5977747d5c8e3a273
Kylewercoir <Kylewercoir@gmail.com>
2025-09-30 20:39:12 -0400

    fixing a mistake i noticed

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php include "inc/header.php";
 include_once "order.php"; ?>
  <link rel="stylesheet" href="css/style.css">
-<div class="text-center mt-5">
+<div class="text">
   <h1>Welcome to Kyles Pizza Order Assignment</h1>
   <p>Click below to order your pizza </p>
   <a href="form.php" class="btn btn-lg btn-primary">Order Pizza</a>
