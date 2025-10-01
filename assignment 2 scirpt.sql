f9a8e440bcb6ad8493eb4e61bf68a7a9021ce025
Kylewercoir <Kylewercoir@gmail.com>
2025-09-30 21:05:36 -0400

    adding deep dish to the script for you i forgot to run it  with it

--- assignment 2 scirpt.sql
+++ assignment 2 scirpt.sql
@@ -5,8 +5,4 @@ CREATE TABLE orders (
     phone VARCHAR(20) NOT NULL,
     pizza_size ENUM('Small','Medium','Large') NOT NULL,
     toppings TEXT,
-    crust ENUM('Thin','Regular','Thick') NOT NULL,
-    notes TEXT,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-
+    crust ENUM('Thin','Regular','Thick',"Deep Dish"
