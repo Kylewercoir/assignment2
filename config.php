05c828f7b0d89b137ad91164a32ab61e7a3a4423
Kylewercoir <Kyle.Connor@MyGeorgian.ca>
2025-09-30 20:48:17 -0400

    taking out my server info config.php

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 // Database connection settings
 define("DB_HOST", "172.31.22.43");
-define("DB_NAME", "Kyle200431463");
-define("DB_USER", "Kyle200431463");
-define("DB_PASS", "jVwfoehVCC");
+define("DB_NAME", "");
+define("DB_USER", ");
+define("DB_PASS", ");
 
